858a8efd21ada29e16ea464e155c1381bfc8846a
Tom <tomlevasseur14@gmail.com>
2026-09-12 18:15:26 +0200

    Install GStreamer tools in Linux CI

--- scripts/install-linux-build-deps.sh
+++ scripts/install-linux-build-deps.sh
@@ -37,6 +37,7 @@ fi
   gstreamer1.0-plugins-bad \
   gstreamer1.0-plugins-base \
   gstreamer1.0-plugins-good \
+  gstreamer1.0-tools \
   gstreamer1.0-nice \
   gstreamer1.0-pipewire \
   gstreamer1.0-vaapi \
